7028805c360ae5e9283ae57e6e548c5084858174
Ondrej Zdych <zdych.ondrej@gmail.com>
2017-07-17 09:49:52 +0200

    chore: update dependencies

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var parseScope = require('lexical-scope');
+var parseScope = require('@zdychacek/lexical-scope');
 var through = require('through2');
 var merge = require('xtend');
 
@@ -53,23 +53,23 @@ var defaultVars = {
 module.exports = function (file, opts) {
     if (/\.json$/i.test(file)) return through();
     if (!opts) opts = {};
-    
+
     var basedir = opts.basedir || '/';
     var vars = merge(defaultVars, opts.vars);
     var varNames = Object.keys(vars).filter(function(name) {
         return typeof vars[name] === 'function';
     });
-    
+
     var quick = RegExp(varNames.map(function (name) {
         return '\\b' + name + '\\b';
     }).join('|'));
-    
+
     var chunks = [];
-    
+
     return through(write, end);
-    
+
     function write (chunk, enc, next) { chunks.push(chunk); next() }
-    
+
     function end () {
         var self = this;
         var source = Buffer.isBuffer(chunks[0])
@@ -79,18 +79,27 @@ module.exports = function (file, opts) {
         source = source
             .replace(/^\ufeff/, '')
             .replace(/^#![^\n]*\n/, '\n');
-        
+
         if (opts.always !== true && !quick.test(source)) {
             this.push(source);
             this.push(null);
             return;
         }
-        
+
         try {
             var scope = opts.always
                 ? { globals: { implicit: varNames } }
-                : parseScope('(function(){\n' + source + '\n})()')
-            ;
+                : parseScope('(function(){\n' + source + '\n})()', {
+					plugins: [
+						'objectRestSpread',
+						'decorators',
+						'classProperties',
+						'classProperties',
+						'functionBind'
+					]
+				});
+
+			//console.log(scope.globals)
         }
         catch (err) {
             var e = new SyntaxError(
@@ -100,9 +109,9 @@ module.exports = function (file, opts) {
             e.filename = file;
             return this.emit('error', e);
         }
-        
+
         var globals = {};
-        
+
         varNames.forEach(function (name) {
             if (!/\./.test(name)) return;
             var parts = name.split('.')
@@ -123,7 +132,7 @@ module.exports = function (file, opts) {
             globals[name] = value;
             self.emit('global', name);
         });
-        
+
         this.push(closeOver(globals, source, file, opts));
         this.push(null);
     }
@@ -135,7 +144,7 @@ function closeOver (globals, src, file, opts) {
     var keys = Object.keys(globals);
     if (keys.length === 0) return src;
     var values = keys.map(function (key) { return globals[key] });
-    
+
     var wrappedSource;
     if (keys.length <= 3) {
         wrappedSource = '(function (' + keys.join(',') + '){\n'
